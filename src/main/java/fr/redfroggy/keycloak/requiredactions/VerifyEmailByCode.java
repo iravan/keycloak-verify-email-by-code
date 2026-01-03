224a6b11d36573abe7ad8fa3fac93ed2bccc3a8a
Ivan Ho <iravan@users.noreply.github.com>
2026-01-03 19:01:44 +0800

    update resend feature

--- src/main/java/fr/redfroggy/keycloak/requiredactions/VerifyEmailByCode.java
+++ src/main/java/fr/redfroggy/keycloak/requiredactions/VerifyEmailByCode.java
@@ -26,6 +26,7 @@ import org.keycloak.authentication.RequiredActionContext;
 import org.keycloak.authentication.RequiredActionFactory;
 import org.keycloak.authentication.RequiredActionProvider;
 import org.keycloak.common.util.SecretGenerator;
+import org.keycloak.common.util.Time;
 import org.keycloak.email.EmailException;
 import org.keycloak.email.EmailTemplateProvider;
 import org.keycloak.email.freemarker.beans.ProfileBean;
@@ -60,6 +61,9 @@ public class VerifyEmailByCode implements RequiredActionProvider, RequiredAction
     public static final String CONFIG_CODE_LENGTH = "code-length";
     public static final String CONFIG_CODE_SYMBOLS = "code-symbols";
     public static final int DEFAULT_CODE_LENGTH = 8;
+    private static final String LAST_OTP_SENT = "last_otp_sent";
+    private static final String OTP_SENT = "otp_sent";
+    private static final long DEBOUNCE_SECONDS = 30;
     public static final String DEFAULT_CODE_SYMBOLS = String.valueOf(SecretGenerator.ALPHANUM);
     private static final Logger logger = Logger.getLogger(VerifyEmailByCode.class);
     private int codeLength;
@@ -68,7 +72,7 @@ public class VerifyEmailByCode implements RequiredActionProvider, RequiredAction
     private static void createFormChallenge(RequiredActionContext context, FormMessage errorMessage) {
         LoginFormsProvider loginFormsProvider = context.form();
         if (Objects.nonNull(errorMessage)) {
-            loginFormsProvider = loginFormsProvider.addError(new FormMessage(EMAIL_CODE, INVALID_CODE));
+            loginFormsProvider = loginFormsProvider.addError(errorMessage);
         }
         Response challenge = loginFormsProvider
                 .setAttribute("user", new ProfileBean(context.getUser(), context.getSession()))
@@ -81,6 +85,8 @@ public class VerifyEmailByCode implements RequiredActionProvider, RequiredAction
         if (context.getRealm().isVerifyEmail()
                 && !context.getUser().isEmailVerified()) {
             context.getUser().addRequiredAction(VERIFY_EMAIL_CODE);
+            context.getAuthenticationSession().removeAuthNote(LAST_OTP_SENT);
+            context.getAuthenticationSession().removeAuthNote(OTP_SENT);
             logger.debug("User is required to verify email");
         }
     }
@@ -99,29 +105,68 @@ public class VerifyEmailByCode implements RequiredActionProvider, RequiredAction
             return;
         }
 
-        sendVerifyEmailAndCreateForm(context);
+        AuthenticationSessionModel authSession = context.getAuthenticationSession();
+        if (authSession.getAuthNote(OTP_SENT) == null && canSendOtp(authSession)) {
+            sendVerifyEmailAndCreateForm(context);
+            authSession.setAuthNote(OTP_SENT, "true");
+            logger.info("Initial OTP sent for user " + context.getUser().getUsername());
+        } else if (!canSendOtp(authSession)) {
+            long secondsRemaining = getSecondsRemaining(authSession);
+            createFormChallenge(context, new FormMessage(null, "error.debounce", String.valueOf(secondsRemaining)));
+        } else {
+            createFormChallenge(context, null);
+        }
     }
 
     @Override
     public void processAction(RequiredActionContext context) {
-        EventBuilder event = context.getEvent().clone().event(EventType.VERIFY_EMAIL).detail(Details.EMAIL, context.getUser().getEmail());
-        String code = context.getAuthenticationSession().getAuthNote(VERIFY_EMAIL_CODE);
-        if (code == null) {
-            requiredActionChallenge(context);
-            return;
-        }
+        EventBuilder event = context.getEvent().clone().event(EventType.VERIFY_EMAIL).detail(Details.EMAIL,
+                context.getUser().getEmail());
+        AuthenticationSessionModel authSession = context.getAuthenticationSession();
         MultivaluedMap<String, String> formData = context.getHttpRequest().getDecodedFormParameters();
-        String emailCode = formData.getFirst(EMAIL_CODE);
+        String action = formData.getFirst("action");
 
-        if (!code.equals(emailCode)) {
-            createFormChallenge(context, new FormMessage(EMAIL_CODE, INVALID_CODE));
-            event.error(INVALID_CODE);
-            return;
+        if ("resend".equals(action)) {
+            if (canSendOtp(authSession)) {
+                sendVerifyEmailAndCreateForm(context);
+                authSession.setAuthNote(OTP_SENT, "true");
+                logger.info("OTP resent for user " + context.getUser().getUsername());
+                event.success();
+            } else {
+                long secondsRemaining = getSecondsRemaining(authSession);
+                createFormChallenge(context, new FormMessage(null, "error.debounce", String.valueOf(secondsRemaining)));
+                logger.info("Resend rejected for user " + context.getUser().getUsername() +
+                        " (" + secondsRemaining + "s remaining)");
+                event.error("debounce_rejected");
+            }
+        } else {
+            String code = formData.getFirst(EMAIL_CODE);
+            String storedCode = authSession.getAuthNote(VERIFY_EMAIL_CODE);
+            if (code == null || !code.equals(storedCode)) {
+                createFormChallenge(context, new FormMessage(EMAIL_CODE, INVALID_CODE));
+                event.error(INVALID_CODE);
+            } else {
+                context.getUser().setEmailVerified(true);
+                authSession.removeAuthNote(VERIFY_EMAIL_CODE);
+                authSession.removeAuthNote(LAST_OTP_SENT);
+                authSession.removeAuthNote(OTP_SENT);
+                event.success();
+                context.success();
+            }
         }
-        context.getUser().setEmailVerified(true);
-        context.getAuthenticationSession().removeAuthNote(VERIFY_EMAIL_CODE);
-        event.success();
-        context.success();
+    }
+
+    private boolean canSendOtp(AuthenticationSessionModel authSession) {
+        String lastSent = authSession.getAuthNote(LAST_OTP_SENT);
+        long currentTime = Time.currentTime();
+        long lastTime = lastSent != null ? Long.parseLong(lastSent) : 0;
+        return currentTime - lastTime >= DEBOUNCE_SECONDS;
+    }
+
+    private long getSecondsRemaining(AuthenticationSessionModel authSession) {
+        String lastSent = authSession.getAuthNote(LAST_OTP_SENT);
+        long lastTime = lastSent != null ? Long.parseLong(lastSent) : 0;
+        return lastTime > 0 ? DEBOUNCE_SECONDS - (Time.currentTime() - lastTime) : 0;
     }
 
     @Override
@@ -157,6 +202,7 @@ public class VerifyEmailByCode implements RequiredActionProvider, RequiredAction
         EventBuilder event = context.getEvent().clone().event(EventType.SEND_VERIFY_EMAIL).detail(Details.EMAIL, user.getEmail());
         String code = SecretGenerator.getInstance().randomString(codeLength, codeSymbols.toCharArray());
         authSession.setAuthNote(VERIFY_EMAIL_CODE, code);
+        authSession.setAuthNote(LAST_OTP_SENT, String.valueOf(Time.currentTime()));
         RealmModel realm = session.getContext().getRealm();
 
         Map<String, Object> attributes = new HashMap<>();
--- src/main/resources/theme-resources/templates/login-verify-email-code.ftl
+++ src/main/resources/theme-resources/templates/login-verify-email-code.ftl
@@ -1,17 +1,15 @@
 <#import "template.ftl" as layout>
-<@layout.registrationLayout displayInfo=true displayMessage=!messagesPerField.exists('email_code'); section>
-    <#if section = "header">
-        ${msg("emailVerifyTitle")}
-    <#elseif section = "form">
-        <p class="instruction">${msg("emailVerifyInstruction1", user.email)}</p>
-        <form id="kc-verify-email-code-form" class="${properties.kcFormClass!}" action="${url.loginAction}"
-              method="post">
+<@layout.registrationLayout displayMessage=!messagesPerField.exists('email_code'); section>
+    <#if section = "form">
+        <form id="kc-verify-email-code-form" class="${properties.kcFormClass!}" action="${url.loginAction}" method="post">
             <div class="${properties.kcFormGroupClass!} ${messagesPerField.printIfExists('email_code',properties.kcFormGroupErrorClass!)}">
                 <div class="${properties.kcLabelWrapperClass!}">
-                    <label for="email_code" class="${properties.kcLabelClass!}">${msg("email_code")}</label>
+                    <label for="email_code" class="${properties.kcLabelClass!}">${msg("verifyEmailCode")}</label>
                 </div>
                 <div class="${properties.kcInputWrapperClass!}">
                     <input type="text" id="email_code" name="email_code" class="${properties.kcInputClass!}"
+                           autocomplete="off"
+                           autofocus
                            aria-invalid="<#if messagesPerField.exists('email_code')>true</#if>"
                     />
 
@@ -25,30 +23,21 @@
             </div>
 
             <div class="${properties.kcFormGroupClass!}">
+                <div id="kc-form-buttons" class="${properties.kcFormButtonsClass!}">
+                    <input class="${properties.kcButtonClass!} ${properties.kcButtonPrimaryClass!} ${properties.kcButtonBlockClass!} ${properties.kcButtonLargeClass!}"
+                           type="submit" name="action" value="verify" />
+                    <input class="${properties.kcButtonClass!} ${properties.kcButtonDefaultClass!} ${properties.kcButtonBlockClass!} ${properties.kcButtonLargeClass!}"
+                           type="submit" name="action" value="resend" />
+                </div>
+            </div>
+
+            <div class="${properties.kcFormGroupClass!}">
                 <div id="kc-form-options" class="${properties.kcFormOptionsClass!}">
                     <div class="${properties.kcFormOptionsWrapperClass!}">
+                        <a href="${url.loginRestartFlowUrl}">${msg("doCancel")}</a>
                     </div>
-                </div>
-
-                <div id="kc-form-buttons" class="${properties.kcFormButtonsClass!}">
-                    <#if isAppInitiatedAction??>
-                        <input class="${properties.kcButtonClass!} ${properties.kcButtonPrimaryClass!} ${properties.kcButtonLargeClass!}"
-                               type="submit" value="${msg("doSubmit")}"/>
-                        <button
-                        class="${properties.kcButtonClass!} ${properties.kcButtonDefaultClass!} ${properties.kcButtonLargeClass!}"
-                        type="submit" name="cancel-aia" value="true" />${msg("doCancel")}</button>
-                    <#else>
-                        <input class="${properties.kcButtonClass!} ${properties.kcButtonPrimaryClass!} ${properties.kcButtonBlockClass!} ${properties.kcButtonLargeClass!}"
-                               type="submit" value="${msg("doSubmit")}"/>
-                    </#if>
                 </div>
             </div>
         </form>
-    <#elseif section = "info">
-        <p class="instruction">
-            ${msg("emailVerifyInstruction2")}
-            <br/>
-            <a href="">${msg("doClickHere")}</a> ${msg("emailVerifyInstruction3")}
-        </p>
     </#if>
 </@layout.registrationLayout>
